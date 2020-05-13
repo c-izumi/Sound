5f07abb40cfbe069fa739bca9721458f5b2f7cc6
Chigusa Izumi <chigusa.f124@gmail.com>
2020-05-13 21:43:00 +0900

    サウンドアプリの完成

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -7,14 +7,22 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    let drumSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "drumSound")!.data)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func touchDownDrumButton(){
+        
+        
+        drumSoundPlayer.currentTime = 0
+        
+        drumSoundPlayer.play()
+    }
 }
 
